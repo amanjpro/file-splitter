e7501d5324cf7269a093b91699057ecfe81d6459
amanjpro <amanjpro@gmail.com>
2019-10-06 15:30:38 -0400

    Process command line args

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,23 +1,47 @@
 package me.amanj.file.splitter
 
-import me.amanj.file.splitter.fs.S3
+import me.amanj.file.splitter.fs._
 import me.amanj.file.splitter.syntax.Implicits._
+import me.amanj.file.splitter.args.{ParseArgs, Config}
 import software.amazon.awssdk.regions.Region
+import java.io.{PrintWriter, BufferedReader}
 
 object App {
+  def getPartNames(base: String, sep: String, parts: Int): Seq[String] =
+    (0 to parts).map(i => s"$base$sep$i%05d")
 
-  def main(args: Array[String]): Unit = {
-    val printers = (0 until 10).map { index =>
-      new S3(Region.US_WEST_2)
-        .sink(s"s3://this/$index")
-        .gzip
-        .printer
+  def getSource(config: Config, fs: FS): BufferedReader =
+    config.inputCompression match {
+      case Some("gzip") =>
+        fs.source(config.inputFile).gzip.buffered
+      case _      =>
+        fs.source(config.inputFile).buffered
     }
 
-    new S3(Region.US_WEST_2)
-      .source("s3://here/there")
-      .gzip
-      .buffered
-      .sinks(printers.toArray)
+  def getSinks(config: Config, fileNames: Seq[String], fs: FS): Seq[PrintWriter] =
+    fileNames.map { file =>
+      config.outputCompression match {
+        case Some("gzip") =>
+          fs.sink(file).gzip.printer
+        case _      =>
+          fs.sink(file).printer
+      }
+    }
+
+  def main(args: Array[String]): Unit = {
+    ParseArgs.parser.parse(args, Config()) match {
+      case Some(config) =>
+        val inputFS = getInputFS(config)
+        val outputFS = getOutputFS(config)
+        val partNames =
+          getPartNames(config.outputDir, outputFS.separator,
+            config.numberOfParts)
+
+        val src = getSource(config, inputFS)
+        val dest = getSinks(config, partNames, outputFS)
+
+        src.sinks(dest.toArray)
+      case _            => // do nothing
+    }
   }
 }
