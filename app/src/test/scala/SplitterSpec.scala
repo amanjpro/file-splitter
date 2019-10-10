fa6767951bd4086deed67adc5fe44c7719ac1095
amanjpro <amanjpro@gmail.com>
2019-10-09 22:56:28 -0400

    Add tests for ordered and unordered splitters

---
+++ app/src/test/scala/SplitterSpec.scala
@@ -0,0 +1,56 @@
+package me.amanj.file.splitter
+
+import me.amanj.file.splitter.fs.LocalFS
+import me.amanj.file.splitter.syntax.Implicits._
+import org.scalatest._
+import java.io.{File, PrintWriter, InputStream}
+import java.nio.file.{Files, Path}
+
+class SplitterSpec extends FlatSpec with Matchers with BeforeAndAfterEach {
+  var in: String = _
+  var out1: String = _
+  var out2: String = _
+
+  override def beforeEach(): Unit = {
+    in = Files.createTempFile("test", "input").toString
+    out1 = Files.createTempFile("test", "out1").toString
+    out2 = Files.createTempFile("test", "out2").toString
+
+    val writer = new PrintWriter(in)
+    writer.println("1")
+    writer.println("2")
+    writer.println("3")
+    writer.println("4")
+    writer.close
+    super.beforeEach
+  }
+
+  override def afterEach(): Unit = {
+    new File(in).delete
+    new File(out1).delete
+    new File(out2).delete
+    super.afterEach
+  }
+
+  "Ordered.split" should "keep the input order when splitting" in {
+    new Splitter.Ordered(LocalFS.source(in).buffered, 4)
+      .sinks(Array(new PrintWriter(out1), new PrintWriter(out2)))
+
+    val lines1 = scala.io.Source.fromFile(out1).getLines.toSeq
+    val lines2 = scala.io.Source.fromFile(out2).getLines.toSeq
+
+    lines1 shouldBe Seq("1", "2")
+    lines2 shouldBe Seq("3", "4")
+  }
+
+  "Unordered.split" should "use one printer per line" in {
+    LocalFS.source(in).buffered
+      .sinks(Array(new PrintWriter(out1), new PrintWriter(out2)))
+
+    val lines1 = scala.io.Source.fromFile(out1).getLines.toSeq
+    val lines2 = scala.io.Source.fromFile(out2).getLines.toSeq
+
+    lines1 shouldBe Seq("1", "3")
+    lines2 shouldBe Seq("2", "4")
+  }
+}
