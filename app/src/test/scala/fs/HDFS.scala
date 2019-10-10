50aeb8050ba47cda823b7e98f1e54b3e489528cb
amanjpro <amanjpro@gmail.com>
2019-10-10 00:44:40 -0400

    Add some tests for HDFS

---
+++ app/src/test/scala/fs/HDFS.scala
@@ -0,0 +1,18 @@
+package me.amanj.file.splitter.fs
+
+import org.scalatest._
+
+class HDFSSpec extends FlatSpec with Matchers {
+  "separator" should "be /" in {
+    new HDFS().separator shouldBe "/"
+  }
+
+  "extractFilePath" should "remove leading hdfs:// from path" in {
+    new HDFS().extractFilePath("hdfs://file_name") shouldBe "file_name"
+  }
+
+  "extractFilePath" should "not remove non-leading hdfs:// from path" in {
+    new HDFS().extractFilePath("hdfs://hdfs://file_name") shouldBe "hdfs://file_name"
+  }
+}
+
