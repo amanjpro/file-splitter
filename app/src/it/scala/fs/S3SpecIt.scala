5db44d045f34abc5dc13bbc24b2d9cf0b3a29154
amanjpro <amanjpro@gmail.com>
2019-10-20 19:04:15 -0400

    Add integration test for S3

---
+++ app/src/it/scala/fs/S3SpecIt.scala
@@ -0,0 +1,120 @@
+package me.amanj.file.splitter.fs
+
+import org.scalatest._
+import java.net.URI
+import java.lang.Boolean.TRUE
+import java.io.{File, PrintWriter, BufferedReader, InputStreamReader}
+import java.nio.file.Files
+import java.nio.file.{Files, Path}
+
+// S3 imports
+import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials,
+  StaticCredentialsProvider}
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.{CreateBucketRequest,
+  PutObjectRequest, DeleteBucketRequest}
+import software.amazon.awssdk.http.SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient
+import software.amazon.awssdk.utils.AttributeMap
+
+// Java interop
+import scala.collection.JavaConverters._
+
+class S3SpecIt extends FlatSpec with
+  Matchers with BeforeAndAfterEach {
+
+  val credential = AwsBasicCredentials.create("key", "secret")
+
+  var s3Client: S3Client = _
+
+  var in: Path = _
+  var out: Path = _
+
+  override def afterEach(): Unit = {
+    s3Client.deleteBucket(
+      DeleteBucketRequest.builder.bucket("foo").build)
+
+    new File(in.toString).delete
+    new File(out.toString).delete
+
+    s3Client.close
+    super.afterEach
+  }
+
+  override def beforeEach(): Unit = {
+    s3Client = S3Client
+      .builder
+      .region(Region.of("us-east-1"))
+      .endpointOverride(URI.create("http://localhost:8001"))
+      .credentialsProvider(
+        StaticCredentialsProvider.create(credential))
+      .httpClient(UrlConnectionHttpClient.builder()
+        .buildWithDefaults(AttributeMap.builder()
+          .put(TRUST_ALL_CERTIFICATES, TRUE)
+        .build()))
+      .build
+
+
+    in = Files.createTempFile("test", "input")
+    out = Files.createTempFile("test", "out")
+
+    val writer = new PrintWriter(in.toString)
+    writer.print(1)
+    writer.close
+
+    s3Client.createBucket(
+      CreateBucketRequest.builder.bucket("foo").build)
+
+    super.beforeEach
+  }
+
+  "exists" should "return false when object is not found" in {
+    new S3(s3Client).exists("s3://foo/bar/baz") shouldBe false
+  }
+
+  "exists" should "return true when object is found" in {
+    s3Client.putObject(
+      PutObjectRequest.builder
+        .bucket("foo")
+        .key("bar/baz").build, in)
+
+    new S3(s3Client).exists("s3://foo/bar/baz") shouldBe true
+  }
+
+  "size" should "return size of the object" in {
+    s3Client.putObject(
+      PutObjectRequest.builder
+        .bucket("foo")
+        .key("bar/baz").build, in)
+    new S3(s3Client).size("s3://foo/bar/baz") shouldBe 1
+  }
+
+  "source" should "should get input stream of path" in {
+    s3Client.putObject(
+      PutObjectRequest.builder
+        .bucket("foo")
+        .key("bar/baz").build, in)
+    val lines = new BufferedReader(
+      new InputStreamReader(
+        new S3(s3Client).source("s3://foo/bar/baz")
+      )
+    ).lines.iterator.asScala.toList
+    lines shouldBe List("1")
+  }
+
+  "sink" should "should get output stream of path" in {
+    val printer = new PrintWriter(
+      new S3(s3Client).sink("s3://foo/bar/baz"))
+    printer.print("2")
+    printer.close
+
+    val lines = new BufferedReader(
+      new InputStreamReader(
+        new S3(s3Client).source("s3://foo/bar/baz")
+      )
+    ).lines.iterator.asScala.toList
+    lines shouldBe List("2")
+  }
+}
+
