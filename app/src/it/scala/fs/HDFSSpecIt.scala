46761cb18fa2ce9bf4cb3d13acb3f8de69d31b00
Amanj Sherwany <sherwany.amanj@gmail.com>
2019-10-21 00:16:22 -0400

    Upkeep/add integration testing for hdfs (#19)
    Add HDFS integration test

---
+++ app/src/it/scala/fs/HDFSSpecIt.scala
@@ -0,0 +1,78 @@
+package me.amanj.file.splitter.fs
+
+import org.scalatest._
+
+import java.nio.file.{Files, Path}
+import java.io.{File, PrintWriter, BufferedReader, InputStreamReader}
+import org.apache.hadoop.fs.{Path => HPath}
+
+// Java interop
+import scala.collection.JavaConverters._
+import org.apache.hadoop.fs.FileSystem
+
+class HDFSSpecIt extends FlatSpec with
+  Matchers with BeforeAndAfterEach {
+
+  val hdfs = new HDFS("hdfs://localhost:9000", "root")
+
+  var in: Path = _
+  var out: Path = _
+
+  override def afterEach(): Unit = {
+    new File(in.toString).delete
+    new File(out.toString).delete
+    super.afterEach
+  }
+
+  override def beforeEach(): Unit = {
+    in = Files.createTempFile("test", "input")
+    out = Files.createTempFile("test", "out")
+
+    val writer = new PrintWriter(in.toString)
+    writer.print(1)
+    writer.close
+
+    super.beforeEach
+  }
+
+  "exists" should "return false when object is not found" in {
+    hdfs.exists("/foo/bar/baz") shouldBe false
+  }
+
+  "exists" should "return true when object is found" in {
+    hdfs.exists("/") shouldBe true
+  }
+
+  "size" should "return size of the object" in {
+    hdfs.fileSystem.copyFromLocalFile(new HPath(in.toString),
+      new HPath("/user/root/test.txt"))
+    hdfs.size("/user/root/test.txt") shouldBe 1
+  }
+
+  "source" should "should get input stream of path" in {
+    hdfs.fileSystem.copyFromLocalFile(new HPath(in.toString),
+      new HPath("/user/root/test.txt"))
+
+    val lines = new BufferedReader(
+      new InputStreamReader(
+        hdfs.source("/user/root/test.txt")
+      )
+    ).lines.iterator.asScala.toList
+    lines shouldBe List("1")
+  }
+
+  "sink" should "should get output stream of path" in {
+    val printer = new PrintWriter(
+      hdfs.sink("/user/root/test2"))
+    printer.print("2")
+    printer.close
+
+    val lines = new BufferedReader(
+      new InputStreamReader(
+        hdfs.source("/user/root/test2")
+      )
+    ).lines.iterator.asScala.toList
+    lines shouldBe List("2")
+  }
+}
+
