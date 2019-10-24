be5f88a4a862d999a8d7833b68b9479d9d8aa782
amanjpro <amanjpro@gmail.com>
2019-10-23 23:30:41 -0400

    Add tests for StdIO in/out methods

--- app/src/test/scala/fs/StdIO.scala
+++ app/src/test/scala/fs/StdIO.scala
@@ -1,6 +1,9 @@
 package me.amanj.file.splitter.fs
 
 import org.scalatest._
+import java.io.{ByteArrayInputStream, BufferedReader,
+  InputStreamReader, OutputStreamWriter, ByteArrayOutputStream,
+  PrintWriter, PrintStream}
 
 class StdIOFSSpec extends FlatSpec with Matchers {
   "separator" should "be empty" in {
@@ -19,6 +22,34 @@ class StdIOFSSpec extends FlatSpec with Matchers {
 
   it should "return true for stdin" in {
     StdIO.exists("stdin") shouldBe true
+  }
+
+  "source" should "read from stdin" in {
+    val inputStr =
+      """|First line
+         |Second line
+         |""".stripMargin
+    // val in = new StringReader(inputStr)
+    var lines = ""
+    System.setIn(new ByteArrayInputStream(inputStr.getBytes()));
+
+    new BufferedReader(new InputStreamReader(StdIO.source("")))
+      .lines().forEach { line =>
+        lines += s"$line\n"
+      }
+    lines shouldBe inputStr
+  }
+
+  "sink" should "write to stdout" in {
+    val baos =new ByteArrayOutputStream
+    val ps = new PrintStream(baos)
+    System.setOut(ps)
+
+
+    val printer = new PrintWriter(new OutputStreamWriter(StdIO.sink("")))
+    printer.print("Yay")
+    printer.close
+    baos.toString shouldBe "Yay"
   }
 }
 
