aeb921fb21ddb0dda5e78ea428bf7e8c18751ec3
amanjpro <amanjpro@gmail.com>
2019-10-26 10:04:03 -0400

    Test if -n option to App.main is respected

---
+++ app/src/it/scala/AppSpecIt.scala
@@ -0,0 +1,53 @@
+package me.amanj.file.splitter
+
+import org.scalatest._
+import java.io.{File, PrintWriter}
+import java.nio.file.{Files, Path}
+
+class AppSpecIt extends FlatSpec with
+  Matchers with BeforeAndAfterEach {
+
+  var in: Path = _
+  var out: Path = _
+  var outDir: Path = _
+
+  override def afterEach(): Unit = {
+    new File(in.toString).delete
+    new File(out.toString).delete
+    new File(outDir.toString).delete
+    super.afterEach
+  }
+
+  override def beforeEach(): Unit = {
+    in = Files.createTempFile("test", "input")
+    out = Files.createTempFile("test", "out")
+    outDir = Files.createTempDirectory("testdir")
+
+    val writer = new PrintWriter(in.toString)
+    writer.println(1)
+    writer.println(2)
+    writer.println(3)
+    writer.println(4)
+    writer.close
+
+    super.beforeEach
+  }
+
+  "App" should "respect -n option" in {
+    val args = Array (
+      "-i", s"file://${in.toString}",
+      "-o", s"file://${outDir.toString}",
+      "-n", "4"
+    )
+
+    // run the application
+    App.main(args)
+
+    val outputFiles = new File(outDir.toString).listFiles
+    outputFiles.length shouldBe 4
+    outputFiles.foreach { file =>
+      file.length shouldBe 2L
+    }
+  }
+}
+
