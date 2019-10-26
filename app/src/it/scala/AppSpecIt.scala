3038b274a075a07811e6664aaa37e77afdbea0c2
amanjpro <amanjpro@gmail.com>
2019-10-26 17:21:57 -0400

    Add more tests for compression and application  itself

--- app/src/it/scala/AppSpecIt.scala
+++ app/src/it/scala/AppSpecIt.scala
@@ -1,9 +1,12 @@
 package me.amanj.file.splitter
 
-import org.scalatest._
-import java.io.{File, PrintWriter}
+import me.amanj.file.splitter.compression.Gzip
+import java.io.{File, PrintWriter, FileInputStream}
 import scala.io.Source
 import java.nio.file.{Files, Path}
+import java.security.Permission
+
+import org.scalatest._
 
 class AppSpecIt extends FlatSpec with
   Matchers with BeforeAndAfterEach {
@@ -16,6 +19,7 @@ class AppSpecIt extends FlatSpec with
     new File(in.toString).delete
     new File(out.toString).delete
     new File(outDir.toString).delete
+    System.setSecurityManager(null)
     super.afterEach
   }
 
@@ -69,6 +73,90 @@ class AppSpecIt extends FlatSpec with
 
     firstLines.toList shouldBe List("1", "2")
     secondLines.toList shouldBe List("3", "4")
+  }
+
+  it should "understand -x option" in {
+    val args = Array (
+      "-i", s"file://${getClass.getResource("/test.gz").getFile}",
+      "-o", s"file://${outDir.toString}",
+      "-x", "gzip",
+    )
+
+    // run the application
+    App.main(args)
+
+    val lines =
+      Source.fromFile(s"${outDir.toString}/part-00000").getLines
+
+    lines.toList shouldBe List("1", "2", "3", "4")
+  }
+
+  it should "understand -z option" in {
+    val args = Array (
+      "-i", s"file://${getClass.getResource("/test").getFile}",
+      "-o", s"file://${outDir.toString}",
+      "-z", "gzip",
+    )
+
+    // run the application
+    App.main(args)
+
+    val reader = Gzip.reader(
+      new FileInputStream(s"${outDir.toString}/part-00000.gz"))
+    var actual = ""
+    var next = reader.read
+    while(next != -1) {
+      actual += next.toChar
+      next = reader.read
+    }
+
+    actual shouldBe "1\n2\n3\n4\n"
+  }
+
+  sealed case class ExitException(status: Int)
+    extends SecurityException("System.exit() is not allowed")
+
+  sealed class NoExitSecurityManager extends SecurityManager {
+    override def checkPermission(perm: Permission): Unit = {}
+    override def checkPermission(perm: Permission,
+      context: Object): Unit = {}
+
+    override def checkExit(status: Int): Unit = {
+      super.checkExit(status)
+      throw ExitException(status)
+    }
+  }
+
+  it should "fail if input does not exist" in {
+    val args = Array (
+      "-i", s"file://${outDir.toString}/nope}",
+      "-o", s"file://${outDir.toString}",
+    )
+
+    System.setSecurityManager(new NoExitSecurityManager())
+    try {
+      // run the application
+      App.main(args)
+    } catch {
+      case e: ExitException =>
+        e.status shouldBe 1
+    }
+  }
+
+  it should "fail if output exists" in {
+    val args = Array (
+      "-i", s"file://${in.toString}",
+      "-9", s"file://${in.toString}",
+    )
+
+    System.setSecurityManager(new NoExitSecurityManager())
+    try {
+      // run the application
+      App.main(args)
+    } catch {
+      case e: ExitException =>
+        e.status shouldBe 1
+    }
   }
 }
 
--- app/src/test/scala/compression/GzipSpec.scala
+++ app/src/test/scala/compression/GzipSpec.scala
@@ -5,15 +5,27 @@ import java.nio.file.Files
 import org.scalatest._
 
 class GzipSpec extends FlatSpec with Matchers {
-  "compressionFactor" should "10" in {
+  "compressionFactor" should "be 10" in {
     Gzip.compressionFactor shouldBe 10
   }
 
-  "extension" should ".gz" in {
+  "extension" should "be .gz" in {
     Gzip.extension shouldBe ".gz"
   }
 
-  "gzip" should "be able to read what it writes" in {
+  "gzip" should "be able to read what gzip files" in {
+    val reader = Gzip.reader(getClass.getResourceAsStream("/test.gz"))
+    var next = reader.read
+    var actual = ""
+    while(next != -1) {
+      actual += next.toChar
+      next = reader.read
+    }
+    actual shouldBe "1\n2\n3\n4\n"
+
+  }
+
+  it should "be able to read what it writes" in {
     val file = Files.createTempFile("test", "file")
     val writer = Gzip.writer(new FileOutputStream(file.toString))
     writer.write("hello world")
