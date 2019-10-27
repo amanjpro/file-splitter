8cd283ec39dfb6b46d4c4377ee2f2a341db1573a
amanjpro <amanjpro@gmail.com>
2019-10-27 12:51:27 -0400

    Move App.scala methods around for clarity

--- app/src/main/scala/App.scala
+++ app/src/main/scala/App.scala
@@ -7,68 +7,70 @@ import me.amanj.file.splitter.args.{Args, Config}
 import java.io.{PrintWriter, BufferedReader}
 
 object App {
-  def getPartNames(base: String, sep: String,
+  private[splitter] def getPartNames(base: String, sep: String,
       parts: Int, ext: String): Seq[String] =
     (0 until parts).map(i => f"$base${sep}part-$i%05d$ext")
 
-  def getSource(compression: Compression,
+  private[splitter] def getSource(compression: Compression,
       input: String, fs: FS): BufferedReader = {
     val path = fs.extractFilePath(input)
     compression.reader(fs.source(path)).buffered
   }
 
-  def getSinks(compression: Compression,
+  private[splitter] def getSinks(compression: Compression,
       fileNames: Seq[String], fs: FS): Seq[PrintWriter] =
     fileNames.map { file =>
       val path = fs.extractFilePath(file)
       compression.writer(fs.sink(s"$path")).printer
     }
 
-  def main(args: Array[String]): Unit = {
-    Args.parser.parse(args, Config()) match {
-      case Some(config) =>
-        // input stream
-        val input = config.input
-        val inputFS = getInputFS(config)
-        val inFile = inputFS.extractFilePath(input)
+  private[splitter] def run(config: Config): Unit = {
+    // input stream
+    val input = config.input
+    val inputFS = getInputFS(config)
+    val inFile = inputFS.extractFilePath(input)
 
-        if(!inputFS.exists(inFile)) {
-          println(s"$inFile does not exist, quitting...")
-          System.exit(1)
-        }
+    if(!inputFS.exists(inFile)) {
+      println(s"$inFile does not exist, quitting...")
+      System.exit(1)
+    }
 
-        val inCompression =
-          Compression.toCompression(config.inputCompression)
-        val source = getSource(inCompression, input, inputFS)
+    val inCompression =
+      Compression.toCompression(config.inputCompression)
+    val source = getSource(inCompression, input, inputFS)
 
-        // output streams
-        val output = config.output
-        val outputFS = getOutputFS(config)
-        val outCompression =
-          Compression.toCompression(config.outputCompression)
-        val partNames =
-          getPartNames(output, outputFS.separator,
-            config.numberOfParts, outCompression.extension)
+    // output streams
+    val output = config.output
+    val outputFS = getOutputFS(config)
+    val outCompression =
+      Compression.toCompression(config.outputCompression)
+    val partNames =
+      getPartNames(output, outputFS.separator,
+        config.numberOfParts, outCompression.extension)
 
-        if(partNames.forall(! outputFS.exists(_))) {
-          val dest = getSinks(outCompression, partNames, outputFS)
-          if(config.keepOrder)
-            source
-              .ordered(
-                inCompression.compressionFactor * inputFS.size(inFile))
-              .sinks(dest.toArray)
-          else
-            source
-              .unordered
-              .sinks(dest.toArray)
-        } else {
-          partNames.filter(outputFS.exists(_)).foreach { part =>
-            println(s"$part exists, cannot override...")
-          }
-          println("quitting")
-          System.exit(2)
-        }
-      case _            => // do nothing
+    if(partNames.forall(! outputFS.exists(_))) {
+      val dest = getSinks(outCompression, partNames, outputFS)
+      if(config.keepOrder)
+        source
+          .ordered(
+            inCompression.compressionFactor * inputFS.size(inFile))
+          .sinks(dest.toArray)
+      else
+        source
+          .unordered
+          .sinks(dest.toArray)
+    } else {
+      partNames.filter(outputFS.exists(_)).foreach { part =>
+        println(s"$part exists, cannot override...")
+      }
+      println("quitting")
+      System.exit(2)
     }
   }
+
+  def main(args: Array[String]): Unit =
+    Args
+      .parser
+      .parse(args, Config())
+      .map(run)
 }
